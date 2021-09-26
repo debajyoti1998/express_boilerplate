dde5151b5bf32366f23bf5ccd7ac8220a79e6819
debajyoti <debajyotipanda1998@gmail.com>
2021-09-26 15:49:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const https = require('https');
+const fs = require('fs');
+const express=require("express");
+const cors = require('cors')
+const cookieParser = require('cookie-parser');  
+const mongoose=require('mongoose');
+
+/**
+  * middleware
+**/
+mongoose.connect(process.env.MONGODB_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>console.log('database connected'))
+.catch(err=>{
+  console.log(err)
+  console.log('connection faild')
+})
+
+/**
+  * middleware
+**/
+
+const app=express()
+var corsOptions = {
+  origin: "https://localhost:3000",
+  credentials: true,
+}
+app.use(cors(corsOptions))
+app.use(express.json());   //bodyparser : old
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+/**
+  * routes
+**/
+app.get('/', async function(req,res){
+  res.send({status:"ping"})
+});
+app.use('/v1',require('./routes/v1'))
+
+app.use(require('./middlewares/handleErrors'))
+
+/**
+  * running server with HTTPS
+**/
+const PORT=process.env.PORT || 8000
+const options = {
+  key: fs.readFileSync('./cert/key.pem'), // Replace with the path to your key
+  cert: fs.readFileSync('./cert/cert.pem') // Replace with the path to your certificate
+}
+https.createServer(options, app).listen(PORT,() => {
+    console.log(`server started : https://localhost:${PORT}`);
+});
